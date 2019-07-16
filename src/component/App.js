2a3aac2dbf4f2e0505911f0d2730b876deca2495
Jayamurugan, Muthukumar <212701120@ge.com>
2019-07-16 14:29:07 +0530

    Examples

--- src/component/App.js
+++ src/component/App.js
@@ -8,11 +8,11 @@ import {
 // App components
 import Header from './Header';
 import Home from './Home';
-import About from './About';
-import Teachers from './Teachers';
-import Courses from './Courses';
+import Jquery from './Jquery';
+// import Teachers from './Teachers';
+// import Courses from './Courses';
 import NotFound from './NotFound';
-import Featured from './Featured';
+// import Featured from './Featured';
 
 const App = () => (
   <BrowserRouter>
@@ -21,10 +21,10 @@ const App = () => (
       
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/about" render={ () => <About title='About' /> } />
-        <Route exact path="/teachers" component={Teachers} />
-        <Route path="/teachers/:topic/:name" component={Featured} />
-        <Route path="/courses" component={Courses} />
+        <Route path="/about" render={ () => <Jquery title='About' /> } />
+        {/* <Route exact path="/teachers" component={Teachers} /> */}
+        {/* <Route path="/teachers/:topic/:name" component={Featured} />
+        <Route path="/courses" component={Courses} /> */}
         <Route component={NotFound} />
       </Switch>
     </div>
--- src/component/Home.js
+++ src/component/Home.js
@@ -19,11 +19,6 @@ class Home extends Component {
         <p>We have thousands of videos created by expert teachers on web design and front end development. Our library is continually refreshed with the latest on web technology so you will never fall behind.</p>
         <hr />
         <h3>Featured Teachers</h3>
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" placeholder="Name" ref={ (input) => this.name = input } />
-          <input type="text" placeholder="Topic" ref={ (input) => this.topic = input } />
-          <button type="submit">Go!</button>
-        </form>
       </div>
     );
   }
